80d61d22ed6c9fa6f9bb79b6a410eecf7eb514c6
non_hana <nonhana@outlook.com>
2025-09-19 20:26:35 +0800

    feat: primary color

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,18 @@
 'use client'
 
+import { AppProgressProvider } from '@bprogress/next'
 import { HeroUIProvider } from '@heroui/react'
+import { ThemeProvider } from 'next-themes'
 import { useRouter } from 'next/navigation'
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter()
 
-  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>
+  return (
+    <HeroUIProvider navigate={router.push}>
+      <ThemeProvider attribute="class">
+        <AppProgressProvider>{children}</AppProgressProvider>
+      </ThemeProvider>
+    </HeroUIProvider>
+  )
 }
